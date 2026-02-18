ff3582c635a0ac85b3147e843abb12f777dbeb9b
Shahidul Islam <shahidul2k9@gmail.com>
2026-02-17 18:24:58 -0600

    fix fanout method filtering

--- co-evolution/src/ptc/generator/generate_pt_change.py
+++ co-evolution/src/ptc/generator/generate_pt_change.py
@@ -27,21 +27,23 @@ for tooName in os.listdir(f"{CACHE_DIRECTORY}/history"):
         if os.path.exists(pt_link_file):
             pt_link_df = pd.read_csv(pt_link_file, keep_default_na=False, na_filter=False)
 
-            # identify rows that are unique w.r.t caller or callee
-            unique_mask = (
-                    ~pt_link_df.duplicated(subset="caller_url", keep=False) |
-                    ~pt_link_df.duplicated(subset="callee_url", keep=False)
-            )
-
-            # rows where link_nc or link_ncc is 1
-            link_mask = (pt_link_df["link_nc"] == 1) | (pt_link_df["link_ncc"] == 1)
-
-            # keep rows that satisfy either condition
-            pt_link_df = pt_link_df[unique_mask | link_mask]
+            # # identify rows that are unique w.r.t caller or callee
+            # unique_mask = (
+            #         ~pt_link_df.duplicated(subset="caller_url", keep=False) |
+            #         ~pt_link_df.duplicated(subset="callee_url", keep=False)
+            # )
+            #
+            # # rows where link_nc or link_ncc is 1
+            # link_mask = (pt_link_df["link_nc"] == 1) | (pt_link_df["link_ncc"] == 1)
+            #
+            # # keep rows that satisfy either condition
+            # pt_link_df = pt_link_df[unique_mask | link_mask]
 
             for link_strategy in LinkStrategy:
                 for tool_name in history_df["tool_name"].unique():
                     if link_strategy.value == "lc":
+                        score_cols = ["link_lc"]
+                    elif link_strategy.value == "max":
                         score_cols = [
                             "link_nc",
                             "link_ncc",
@@ -49,8 +51,6 @@ for tooName in os.listdir(f"{CACHE_DIRECTORY}/history"):
                             "link_lcs_u",
                             "link_leven",
                         ]
-                    elif link_strategy.value == "max":
-                        score_cols = ["link_lc"]
                     else:
                         raise ValueError(f"Unknown link strategy: {link_strategy}")
 
@@ -60,9 +60,7 @@ for tooName in os.listdir(f"{CACHE_DIRECTORY}/history"):
                     pt_link_change_df = (pt_link_df.merge(tool_df.add_prefix("caller_"), on="caller_url", how="inner")
                      .merge(tool_df.add_prefix("callee_"), on="callee_url", how="inner"))
 
-                    pt_link_change_df = (pt_link_change_df[((pt_link_change_df["caller_method_type"] == "test")
-                                                            | (pt_link_change_df["caller_method_type"] == "test_util"))
-                                                           & (pt_link_change_df["callee_method_type"] == "production")])
+                    pt_link_change_df = (pt_link_change_df[(pt_link_change_df["caller_method_type"] == "test") & (pt_link_change_df["callee_method_type"] == "production")])
 
                     pt_link_change_df["_row_id"] = pt_link_change_df.index
                     best_links_change_df = (
