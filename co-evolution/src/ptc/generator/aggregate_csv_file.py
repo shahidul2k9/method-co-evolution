1355b5cfacfa54099f5cb9c1d55dc0e4cd4369d8
shahidul <shahidul2k9@gmail.com>
2026-03-01 11:10:15 -0600

    evalute result with ground truth

--- co-evolution/src/ptc/generator/aggregate_csv_file.py
+++ co-evolution/src/ptc/generator/aggregate_csv_file.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 
-def aggregate_csv_files(input_dir: str, output_file_name: str):
+def aggregate_csv_files(input_dir, output_file_name: str):
     dfs = [pd.read_csv(file, keep_default_na=False, na_filter=False) for
            file in list(Path(input_dir).rglob("*.csv"))]
     dfs = [d for d in dfs if not d.empty]
@@ -13,9 +13,15 @@ def aggregate_csv_files(input_dir: str, output_file_name: str):
         df.to_csv(f"{DATA_DIRECTORY}/aggregate/{output_file_name}", index=False)
 
 
-aggregate_csv_files(f"{DATA_DIRECTORY}/m2m-confidence", "m2m-confidence.csv")
-t2p_strategy_dirs = [dir_name for dir_name in os.listdir(f"{DATA_DIRECTORY}/t2p-link") if
-                     os.path.isdir(f"{DATA_DIRECTORY}/t2p-link/{dir_name}")]
-for t2p_strategy in t2p_strategy_dirs:
-    aggregate_csv_files(f"{DATA_DIRECTORY}/t2p-link/{t2p_strategy}",
-                        f"t2p-link--{t2p_strategy}.csv")
+# aggregate_csv_files(f"{DATA_DIRECTORY}/m2m-confidence", "m2m-confidence.csv")
+# t2p_strategy_dirs = [dir_name for dir_name in os.listdir(f"{DATA_DIRECTORY}/t2p-link") if
+#                      os.path.isdir(f"{DATA_DIRECTORY}/t2p-link/{dir_name}")]
+#
+# for t2p_strategy_dir in t2p_strategy_dirs:
+#     aggregate_csv_files(f"{DATA_DIRECTORY}/t2p-link/{t2p_strategy_dir}",
+#                         f"t2p-link--{t2p_strategy_dir}.csv")
+
+for t2p_strategy_dir in Path(f"{DATA_DIRECTORY}/t2p-link-missmatch").iterdir():
+    if t2p_strategy_dir.is_dir():
+        aggregate_csv_files(t2p_strategy_dir,
+                        f"t2p-link-missmatch--{t2p_strategy_dir.name}.csv")
